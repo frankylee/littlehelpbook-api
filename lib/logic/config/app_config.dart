ba30c56de4af1dd36f2339cb1b8d513ff82fd1df
frankylee <frankyleekelly@gmail.com>
2023-02-07 08:23:29 -0800

    Add App Config to access environment configs
    This allows the app to access secrets and configs from the `.env`.
    
    This has additional helpers to easily check environment and log the values to the console during development.
    
    <!-- ps-id: de23fadb-6e0a-4418-bda1-70c41128e9f9 -->

---
+++ lib/logic/config/app_config.dart
@@ -0,0 +1,33 @@
+import 'package:envied/envied.dart';
+
+part 'app_config.g.dart';
+
+@Envied(obfuscate: true, requireEnvFile: true)
+abstract class AppConfig {
+  @EnviedField(varName: 'ENV', defaultValue: 'dev')
+  static final env = _AppConfig.env;
+
+  static bool get isDev => env.contains('dev');
+  static bool get isProd => env.contains('prod');
+  static bool get isStage => env.contains('stage');
+
+  @EnviedField(varName: 'AIRTABLE_API_KEY')
+  static final airtableApiKey = _AppConfig.airtableApiKey;
+
+  @EnviedField(varName: 'AIRTABLE_BASE_ID')
+  static final airtableBaseId = _AppConfig.airtableBaseId;
+
+  static String toStr() {
+    return '''
+      AppConfig(
+        env: $env
+        isDev: $isDev
+        isProd: $isProd
+        isStage: $isStage
+        ---------------------
+        airtableApiKey: $airtableApiKey
+        airtableBaseId: $airtableBaseId
+      )
+    ''';
+  }
+}
