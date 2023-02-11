9ec368de6143b1edd411eb256e02d43f93846454
frankylee <frankyleekelly@gmail.com>
2023-02-10 22:12:34 -0800

    Add generic exception for explicitly handling errors
    This facilitates explicit error handling and code reuse by allowing all other error types to become subclasses of this superclass.
    
    This class will be used by all other errors classes to make errors more specific and responsible for their domain.
    
    <!-- ps-id: 2e364671-dac6-4af4-8ee4-9ea9a7a42cd2 -->

---
+++ lib/logic/common/error.dart
@@ -0,0 +1,30 @@
+/// Little Help Book Errors can be thrown anywhere in the project, allowing
+/// errors to be explicitly handled and raised.
+///
+/// Generally, this is the generic exception that other exceptions should
+/// implement in order to provide more explicit exceptions to the consumer.
+class LittleHelpBookError implements Exception {
+  LittleHelpBookError([String? message, String? error, int? statusCode]) {
+    if (message != null) this.message = message;
+    if (error != null) this.error = error;
+    if (statusCode != null) this.statusCode = statusCode;
+  }
+
+  String error = 'LittleHelpBookError';
+  String message = 'Internal Server Error';
+  int statusCode = 500;
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{
+      'error': error,
+      'message': message,
+      'statusCode': statusCode,
+    };
+    return data;
+  }
+
+  @override
+  String toString() {
+    return '$error: $message';
+  }
+}
